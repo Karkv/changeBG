14dfc88ae5e95591123446758a2af131b099ddbb
Karan Vishwakarma <108710246+Karkv@users.noreply.github.com>
2025-01-20 17:21:08 +0530

    Update settings.py

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g6*3@)tphsmu6a8^dyu!x82k=_)#plzvf)(q07di=f68p)#ag8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
